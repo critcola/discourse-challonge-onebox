6b60716309a1ea1300d90f0d1f21a3d611b6bf50
Daniel Marquard <daniel@netidy.com>
2017-01-13 02:51:07 -0500

    Initial commit

---
+++ plugin.rb
@@ -0,0 +1,23 @@
+# name: Discourse Challonge Onebox
+# about: Adds support for embedding Challonge tournament brackets within Discourse.
+# version: 0.1
+# authors: Daniel Marquard
+# url: https://github.com/critcola/discourse-challonge-onebox
+
+class Onebox::Engine::ChallongeOnebox
+	include Onebox::Engine
+
+	REGEX = /^https?:\/\/((?:\w+\.)?challonge.com)\/(\w+)/
+	matches_regexp REGEX
+
+	def hostname
+		@url.match(REGEX)[1]
+	end
+	def id
+		@url.match(REGEX)[2]
+	end
+	
+	def to_html
+		"<iframe src=\"//#{hostname}/#{id}/module\" width=\"695\" height=\"500\" frameborder=\"0\" scrolling=\"auto\" allowtransparency=\"true\"></iframe>"
+	end
+end
